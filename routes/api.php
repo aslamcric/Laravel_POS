38b6e2e496104d5c7ad7c6af6ef167cd90cf373e
aslamcric <mdaslamcric@gmail.com>
2025-04-06 13:14:51 +0600

    working on supplier controller

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\api\PurchaReturnController;
 use App\Http\Controllers\api\PurchaseReturnController;
 use App\Http\Controllers\api\purchasesController;
 use App\Http\Controllers\api\vue\CustomerController;
+use App\Http\Controllers\api\vue\SupplierController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -40,3 +41,4 @@ Route::get('purchase', [OrderController::class,'purchase']);
 
 // Api for vue
 Route::apiResource('customers', CustomerController::class);
+Route::apiResource('suppliers', SupplierController::class);
